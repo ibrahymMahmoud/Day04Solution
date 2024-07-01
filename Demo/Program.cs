b5587d48a66a72a0dac28923b78752d453414726
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-07-01 07:58:24 +0300

    function Ref type prameter example2

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,46 +4,76 @@
     {
         // create function to print shape different number of times
         //static void PrintShape(int count = 5, string shape = "+**+") //if want deafualt prameters
-      ///  static void PrintShape(int count , string shape) 
-      ///  {
-      ///      for (int i = 0; i < count; i++) { 
-      ///          Console.WriteLine(shape);
-      ///      }
-      ///  }
-      ///  
-      
+        ///  static void PrintShape(int count , string shape) 
+        ///  {
+        ///      for (int i = 0; i < count; i++) { 
+        ///          Console.WriteLine(shape);
+        ///      }
+        ///  }
+        ///  
+
         //swap by pass value prameter
-    ///    static void SwapingByValue(int y , int x)
-    ///    {
-    ///        Console.WriteLine("swap - pass by value");
-    ///        int temp = y;
-    ///        y = x;
-    ///        x = temp;
-    ///    }
-    ///
-    ///    static void SwapingByRef(ref int y,ref int x)
-    ///    {
-    ///        Console.WriteLine("swap- pass by Ref");
-    ///        int temp = y;
-    ///        y = x;
-    ///        x = temp;
-    ///    }
-    ///    
+        ///    static void SwapingByValue(int y , int x)
+        ///    {
+        ///        Console.WriteLine("swap - pass by value");
+        ///        int temp = y;
+        ///        y = x;
+        ///        x = temp;
+        ///    }
+        ///
+        ///    static void SwapingByRef(ref int y,ref int x)
+        ///    {
+        ///        Console.WriteLine("swap- pass by Ref");
+        ///        int temp = y;
+        ///        y = x;
+        ///        x = temp;
+        ///    }
+        ///    
 
         //function with Ref prameters
         //create function to sum array elements and change first element
-     ///   static int Sum(int[] array) {
-     ///       int sum = 0;
-     ///       if (array != null) { 
-     ///           array[0] = 100;
-     ///           for (int i = 0; i < array.Length; i++)
-     ///           {
-     ///               sum += array[i];
-     ///           }
-     ///       }
-     ///       return sum ;
-     ///   
-     ///   }
+        ///   static int Sum(int[] array) {
+        ///       int sum = 0;
+        ///       if (array != null) { 
+        ///           array[0] = 100;
+        ///           for (int i = 0; i < array.Length; i++)
+        ///           {
+        ///               sum += array[i];
+        ///           }
+        ///       }
+        ///       return sum ;
+        ///   
+        ///   }
+        ///   
+
+        //create function to sum array elements and change on new array element
+      ///       static int SumByvalue(int[] array) {
+      ///           int sum = 0;
+      ///           if (array != null) { 
+      ///               array= new [] {3,5,11,14};
+      ///               for (int i = 0; i < array.Length; i++)
+      ///               {
+      ///                   sum += array[i];
+      ///               }
+      ///           }
+      ///           return sum ;
+      ///       
+      ///       }
+      ///
+      ///  static int SumByRef(int[] array)
+      ///  {
+      ///      int sum = 0;
+      ///      if (array != null)
+      ///      {
+      ///          array = new[] { 3, 5, 3, 5 };
+      ///          for (int i = 0; i < array.Length; i++)
+      ///          {
+      ///              sum += array[i];
+      ///          }
+      ///      }
+      ///      return sum;
+      ///
+      ///  }
         static void Main(string[] args)
         {
             #region Function(protoType,calling)
@@ -98,11 +128,27 @@
             #region function Ref type prameter example
             // no different between call by Ref and by value with Ref prameter 
             //but behavior is different
-          ///  int[] array = { 1 ,5,8,10,20 };
-          ///  int sum = Sum(array);
-          ///  Console.WriteLine($"the sum of array : {sum}");
-          ///  Console.WriteLine($"the first element in  array : {array[0]}");
+            ///  int[] array = { 1 ,5,8,10,20 };
+            ///  int sum = Sum(array);
+            ///  Console.WriteLine($"the sum of array : {sum}");
+            ///  Console.WriteLine($"the first element in  array : {array[0]}");
 
+            #endregion
+
+            #region function Ref type prameter example2
+           /// int[] array = {4,5,11,14,14,14};
+           /// Console.WriteLine("with pass by value");
+           /// int sum = SumByvalue(array);
+           /// Console.WriteLine($" sum of array = {sum}");
+           /// Console.WriteLine("display array after pass by value");
+           /// for(int i =0 ; i < array.Length; i++)
+           ///     Console.WriteLine($" array[{i}]= {array[i]}");
+           /// Console.WriteLine("display by Ref");
+          /// int sum2 = SumByRef(array);
+          /// Console.WriteLine($" sum or array = {sum2}");
+          /// for (int i = 0; i < array.Length; i++)
+          ///     Console.WriteLine($" array[{i}]= {array[i]}");
+          ///
             #endregion
         }
     }
